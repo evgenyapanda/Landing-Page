e581c1a4aac057f398c7793f6daf37daadcdc1cf
evgenyapanda <funeralpanda@mail.ru>
2017-02-23 17:29:02 +0200

    animation in menu

--- js/common.js
+++ js/common.js
@@ -10,17 +10,29 @@ $(document).ready(function() {
 		heightDetect();
 	});
 	
-	$(".toggle_menu, .menu_item").click(function() {
+	$(".toggle_menu").click(function() {
  		$(".sandwich").toggleClass("active");
+	});
+
+	$(".top_mnu ul a").click(function(){
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
 	});
 
 
 	$(".toggle_menu").click(function(){
 		if($(".top_mnu").is(":visible")){
 		   $(".top_mnu").fadeOut(600);
+		   $(".top_mnu li a").removeClass("fadeInUp animated");
 		} else {
 		   $(".top_mnu").fadeIn(600);
+		   $(".top_mnu li a").addClass("fadeInUp animated");
 		};
+	});
+
+	$(".top_mnu li").sliphover({
+		target : "a",
+		backgroundColor : 'rgba(255,255,255,.05)'
 	});
 
 });
