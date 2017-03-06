f07840c210ad7c4c72556d01a8f797f94e3bfd8d
evgenyapanda <funeralpanda@mail.ru>
2017-03-06 17:01:01 +0200

    section portfolio

--- js/common.js
+++ js/common.js
@@ -6,6 +6,10 @@ $(document).ready(function() {
 	$(".animation_3").animated("fadeInRight", "fadeOutRight");
 	$(".animation_2").animated("flipInY", "flipOutY");
 
+	$(".left .resume_item").animated("fadeInLeft", "fadeOutLeft");
+	$(".right .resume_item").animated("fadeInRight", "fadeOutRight");
+
+
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
 
