b9a38863ccf086bcc07ddcba2a5dbce86661dd4e
evgenyapanda <funeralpanda@mail.ru>
2017-03-10 17:34:37 +0200

    portfolio

--- js/common.js
+++ js/common.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
 
+	$("#portfolio_grid").mixItUp();
+
+
+	$(".s_portfolio li").click(function(){
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
 	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:'inline',
+		midClick: true
+	});
 
 	$(".animation_1").animated("fadeInLeft", "fadeOutLeft");
 	$(".animation_3").animated("fadeInRight", "fadeOutRight");
@@ -47,9 +59,16 @@ $(document).ready(function() {
 		};
 	});
 
+	$(".portfolio_item").each(function(i){
+	$(this).find("a").attr("href", "#work_" +i);
+	$(this).find(".port_descr").attr("id", "work_" +i);
+
+	});
+
 
 
 });
+
 
 $(window).load(function() {
   $(".loader_Inner").fadeOut();
